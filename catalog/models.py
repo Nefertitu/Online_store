7909eca2310521152168866c6aac829d1e68e4a3
Olga Kazorina <nefertituolga@gmail.com>
2025-05-04 13:17:24 +0300

    add catalog/migrations/0001_initial.py, add fixes catalog/models.py, pyproject.toml, poetry.lock

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    image = models.ImageField(verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(verbose_name='Цена за покупку', max_digits=20, decimal_places=2)
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения')
+
+
+    def __str__(self):
+        return f'Наименование: {self.name}, цена - {self.price}.'
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ['name', 'price']
+
