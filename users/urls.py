ff7b45b818773febb7f56bcf6796de13da7e4305
Olga Kazorina <nefertituolga@gmail.com>
2025-05-31 22:16:14 +0300

    add register-form(user_form.html)

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
-
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="catalog:product_list"), name="logout"),
-    # path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.core.mail import send_mail
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+from .models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('users:login')
