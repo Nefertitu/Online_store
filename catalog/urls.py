8f3756b1f537197c4f4885d4e65f3b345f8fc853
Olga Kazorina <nefertituolga@gmail.com>
2025-10-08 22:43:44 +0300

    add fixes README.md, .flake8, catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -16,12 +16,12 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("catalog/", ProductsListView.as_view(), name="product_list"),
+    path("catalog/contacts/", contacts, name="contacts"),
     path(
         "catalog/<slug:category_slug>/",
         cache_page(60 * 5)(ProductsOneCategoryList.as_view()),
         name="product_one_category",
     ),
-    path("catalog/contacts/", contacts, name="contacts"),
     path("catalog/<int:pk>/detail/", cache_page(60 * 5)(ProductDetailView.as_view()), name="product_detail"),
     path("catalog/create/", ProductCreateView.as_view(), name="product_form"),
     path("catalog/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
